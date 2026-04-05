35138f78bfae2e4c0a498cb3613a3cdee209deba
MartaBallesterB <martaballesterbadenes@gmail.com>
2026-04-05 19:10:53 +0200

    started with main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Welcome to my Book Recommender Chatbot!")
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+books = pd.read_csv("data/books.csv")
+
+books["combined"] = (books["subjects"].fillna("") + " " + books["description"].fillna(""))
+
+vectorizer = TfidfVectorizer(stop_words="english")
+book_vectors = vectorizer.fit_transform(books["combined"])
 
 
-if __name__ == "__main__":
-    main()
+def recommend_books(query):
+
+    query_vec = vectorizer.transform([query])
+    similarity = cosine_similarity(query_vec, book_vectors)
+    top_indices = similarity.argsort()[0][-5:][::-1]
+
+    return books.iloc[top_indices][["title", "author"]]
